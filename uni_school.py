54fe00f17a5e237496571f1f9ac638a62d5dba66
juliedem94 <deme-yuliya@yandex.ru>
2017-01-29 23:09:48 +0300

    universities for 1000 ids

--- uni_school.py
+++ uni_school.py
@@ -6,7 +6,7 @@ Created on Fri Jan 27 13:49:47 2017
 """
 import vk, math, time, helpers
 
-token = '6c09c77ed5bd767484d66021630c6adfc18ac84ed1edea03218a3f2a9bb6489d3ab570a10dc67d8b0d99a'
+token = '78bc126e0670dacedcc062b905bd83b5e275a5c9e760c60029fc5a4291c9b3c5f2b2f63f0930760b8ad6c'
 session = vk.Session(access_token = token) 
 api = vk.API(session, v = '5.45')
 
@@ -34,23 +34,26 @@ for school_index in school_indexes:
                    
                     break
                 
-    #print(true_users)
-    curr_school_unis=[]
-    for curr_user in true_users:
-        curr_school_user_uni={}
-        time.sleep(0.335)
-        user_uni = api.users.get(user_ids = int(curr_user), fields = 'universities')       
-        for j in range(len(user_uni)):
-            curr_user_uni_list = user_uni[j]
-            uni_info_list = curr_user_uni_list.get('universities')
-            if uni_info_list !=None:
-                for k in range(len(uni_info_list)):                   
-                    curr_school_user_uni[curr_user]=uni_info_list[k]['name']
-                    curr_school_unis.append(curr_school_user_uni)
+                
+    curr_school_unis={}
 
+    true_users_string = ','.join(true_users)
+    time.sleep(0.335)
+    user_uni = api.users.get(user_ids = (true_users_string), fields = 'universities')            
+    
+    for j in range(len(user_uni)):
+        curr_user_uni_list = user_uni[j]
+        curr_user_id=curr_user_uni_list.get('id')
+        uni_info_list = curr_user_uni_list.get('universities')
+            
+        if uni_info_list !=None:
+            for k in range(len(uni_info_list)): 
+                curr_school_unis[curr_user_id]=uni_info_list[k]['name']
+                    
+                    
     helpers.jsonSave('/код/hello_world/data/uni_school_' + str(school_index['School']) + '.json', curr_school_unis)                                             
     print('---------------DONE--------------------')
-
+   
      
 
 
